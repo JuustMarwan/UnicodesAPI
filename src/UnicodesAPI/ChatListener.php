d9635fee67f2ac10824d3a051e15590f21c5efd0
Marwan Elmogazy <mrzinkil2020@gmail.com>
2025-01-21 17:35:32 +0200

    Initial Commit

---
+++ src/UnicodesAPI/ChatListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace UnicodesAPI;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\plugin\Plugin;
+use pocketmine\utils\TextFormat;
+
+use UnicodesAPI\Base;
+
+class ChatListener implements Listener {
+  
+    public function getOwningPlugin(): Plugin {
+      
+        return Base::getInstance();
+    }
+  
+  
+    public function onChat(PlayerChatEvent $event): void {
+      
+        $player = $event->getPlayer();
+        $originalMessage = $event->getMessage();
+
+        if(Base::$autoReplacing) {
+          
+            $newMessage = Base::replace($originalMessage);
+            $event->setMessage($newMessage);
+          
+        }
+    }
+}
